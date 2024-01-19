bdce8fc24d6b73df26cd71538ab4d5b6f9c05d25
sabrerodriguez <sabre.rodriguez@kidstrong.com>
2024-01-19 12:51:55 -0800

    removed unused set

--- src/FormattedTime.js
+++ src/FormattedTime.js
@@ -10,7 +10,6 @@ export default function FormattedDate(props) {
     "Friday",
     "Saturday",
   ];
-  let day = days[props.date.getDay()];
   let hours = props.date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
